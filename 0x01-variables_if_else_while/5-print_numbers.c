2063922acd65e5e14c4093c253cfa990af5c0291
femilana <femilanah@gmail.com>
2023-10-06 16:39:31 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	int digit = 0;
+
+	for (digit = 0; digit <= 9; digit++)
+	{
+		printf("%d", digit);
+	}
+	printf("\n");
+
+	return (0);
+}
