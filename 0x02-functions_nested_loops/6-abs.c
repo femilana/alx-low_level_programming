7d206599c5a8018926fb22839caa103fb178529e
femilana <femilanah@gmail.com>
2023-10-10 18:38:59 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - fuction that computes the absolute value of an integer
+ * @n: input parameter
+ * Return: absolute value of int
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
