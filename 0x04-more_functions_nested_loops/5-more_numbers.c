9ec4700d30bc90750275f64371a9977f164a50df
femilana <femilanah@gmail.com>
2023-10-12 13:54:19 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,15 +14,9 @@ void more_numbers(void)
 	{
 		for (num = 0; num <= 14; num++)
 		{
-			if (num < 10)
-			{
-				_putchar('0' + num);
-			}
-			else
-			{
+			if (num >= 10)
 				_putchar('1');
-				_putchar('0' + (num - 10));
-			}
+			_putchar(num % 10 + '0');
 		}
 		_putchar('\n');
 	}
