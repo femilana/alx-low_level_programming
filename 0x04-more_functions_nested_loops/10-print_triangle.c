5fb257f47ea93df796d6b6b042fafbf89ae7d27d
femilana <femilanah@gmail.com>
2023-10-12 19:26:53 -0700

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_triangle - A function prints a triangle,
+ * followed by a new line
+ * @size: Argument
+ */
+
+void print_triangle(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
+
+		for (i = 1; i <= size; i++)
+		{
+			for (j = i; j < size; j++)
+			{
+				_putchar(' ');
+			}
+
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
+
+			_putchar('\n');
+		}
+	}
+}
