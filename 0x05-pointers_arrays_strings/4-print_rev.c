f5255b66f7973a9b29b3934d3307aed1aff40f4e
femilana <femilanah@gmail.com>
2023-10-16 13:39:34 -0700

    4-print_rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_rev - a function that prints a string,
+ * in reverse, followed by a new line
+ * @s: argument
+ */
+
+void print_rev(char *s)
+{
+	int num = 0;
+
+	int num1;
+
+	while (*s != '\0')
+	{
+		num++;
+		s++;
+	}
+
+	s--;
+
+	for (num1 = num; num1 > 0; num1++)
+	{
+		_putchar(*s);
+		s--;
+	}
+
+	_putchar('n');
+}
