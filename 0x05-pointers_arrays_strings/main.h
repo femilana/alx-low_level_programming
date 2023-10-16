588dcebe47b822af9adadf8edf47ac0c1e1b4fd0
femilana <femilanah@gmail.com>
2023-10-16 08:31:53 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,8 @@
-#ifdef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -12,5 +15,6 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+int _putchar(char c);
 
 #endif
