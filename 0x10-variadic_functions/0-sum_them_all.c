05baaea29a0c8897032805a9e7b531ee322049a7
femilana <femilanah@gmail.com>
2023-11-09 18:49:03 -0800

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,12 +13,14 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 	unsigned int result = 0;
 	va_list mynumber;
-	va_start(mynumber,n);
-	for(i = 0; i < n; i++)
+
+	va_start(mynumber, n);
+
+	for (i = 0; i < n; i++)
 	{
-		result = result + va_arg(mynumber,int);
+		result = result + va_arg(mynumber, int);
 	}
-	
+
 	va_end(mynumber);
-	return result;
+	return (result);
 }
