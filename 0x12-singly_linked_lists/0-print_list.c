76ff42dd94fadec553ed6189565d95f9acbb6a76
femilana <phemeelana@gmail.com>
2024-04-01 06:30:57 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,17 +10,16 @@
 
 size_t print_list(const list_t *h)
 {
- size_t st = 0;
+	size_t st = 0;
 
- while (h)
- {
- if (!h->str)
- printf("[0] (nil)\n");
- else
- printf("[%u] %s\n", h->len, h->str);
- h = h->next;
- st++;
- }
-
- return (st);
+	while (h)
+	{
+		if (!h->str)
+		printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		st++;
+	}
+	return (st);
 }
